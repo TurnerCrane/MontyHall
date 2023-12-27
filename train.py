f39f11176287575bc020e0a4b77ead1e5ef9c89e
TurnerCrane <51405959+TurnerCrane@users.noreply.github.com>
2023-12-28 00:04:26 +0900

    Update train.py

--- train.py
+++ train.py
@@ -4,21 +4,21 @@ from ray.rllib.algorithms.ppo import PPO
 from env.monty_hall_env import MontyHallEnv
 
 def train_monty_hall():
-    ray.init(local_mode=True)
+    ray.init(local_mode=False)
 
     tune.register_env("MontyHallEnv", lambda config: MontyHallEnv(config))
     config = {
       "env": MontyHallEnv,
       "num_workers": 1,
-      "num_gpus": 0,
-      "use_gpu": False,
+      "num_gpus": 1,
+      "use_gpu": True,
       "framework": "torch",
       "env_config": {
         "num_doors": 3,
         "seed": 12345
+      }
     }
-    }
-    stop = {"training_iteration": 200,}
+    stop = {"training_iteration": 10,}
     tune.run(PPO, config=config, stop=stop)
 
 if __name__ == "__main__":
